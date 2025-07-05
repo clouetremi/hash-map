631f70b4b628cb15fcd7c323879bf7d1ba66df0b
Rémi Clouet <clouet.remi@gmail.com>
2025-07-05 14:29:24 +0200

    add 5 premières méthodes

--- hash-map.js
+++ hash-map.js
@@ -22,27 +22,85 @@ class HashMap {
         const index = this.hash(key);
 
         if (index < 0 || index >= this.buckets.length) {
-            throw new Error ("Index is not right");
+            throw new Error("Index is not right");
         }
 
         // Si on a pas de valeur pour cette key (soit pas de donné dans notre buckets)
         // Alors on créer un tableau
         if (!this.buckets[index]) {
-            this.buckets[index] = []; 
+            this.buckets[index] = [];
         }
 
         // Pour chaque pair key/value présent dans notre buckets
         for (let pair of this.buckets[index]) {
             if (pair.key === key) {
                 // la value de pair est la nouvelle
-                pair.value = value; 
-                return; 
+                pair.value = value;
+                return;
             }
         }
 
         // On pousse notre key-/value dans notre buckets à l'index (this.hash)
-        this.buckets[index].push({key, value})
+        this.buckets[index].push({ key, value })
+    };
+
+    get(key) {
+        const index = this.hash(key);
+
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Index is not right");
+        }
+
+        if (!this.buckets[index]) {
+            return null;
+        }
+
+        for (let pair of this.buckets[index]) {
+            if (pair.key === key) {
+                return pair.value;
+            }
+        }
+
+        // Si la clé n'est trouvée dans aucune paire
+        return null;
+    };
+
+    has(key) {
+        const index = this.hash(key);
+
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Index is not right");
+        }
+
+        if (!this.buckets[index]) {
+            return false;
+        }
+
+        for (let pair of this.buckets[index]) {
+            if (pair.key === key) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    remove(key) {
+        const index = this.hash(key);
 
-}
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Index is not right");
+        }
+
+        if (!this.buckets[index]) {
+            return false;
+        }
+
+        for (let i = 0; i < this.buckets[index].length; i++) {
+            const pair = this.buckets[index][i];
+            if (pair.key === key) {
+                this.buckets[index].splice(i, 1); // supprime l'élément
+                return true;
+            }
+        }
+        return false;
+    }
